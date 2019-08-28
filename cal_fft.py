0b360b2d65f5f035765d5b9f122a02375f59b216
bestfocus <51957682+bestfocus@users.noreply.github.com>
2019-08-27 23:16:09 -0400

    Update cal_fft.py

--- cal_fft.py
+++ cal_fft.py
@@ -1,16 +1,29 @@
+#calculate fast fourier transform with scipy.fftpack
 import numpy as np
 import scipy.fftpack
 import matplotlib.pyplot as plt
 
 A=50
 freq=10
+tup=5
 # get t of 200 numbers from 0 to 5
-t = np.linspace(0.0, 5, 200)
-sum_waves=A * np.cos(2 * np.pi * t*freq)
-#sum_waves=A * np.cos(2 * np.pi *t*t)
-# Fast Fourier Transform
-y_fft = scipy.fftpack.fft(sum_waves)
+t = np.linspace(0.0, tup, 200)
+wave=A * np.cos(2 * np.pi * t*freq)
+#wave=A * np.cos(2 * np.pi *t*t)
+#plot wave
+fig, ax = plt.subplots()
+ax.plot(t, wave)
 
-spacing = 5 / 100
+# Fast Fourier Transform
+y_fft = scipy.fftpack.fft(wave)
+
+# plot fft results in frequency dimension
+N=100
 # get x_fft of 100 numbers from 0 to 20
-x_fft = np.linspace(0.0, 1.0/spacing, 100)
+x_fft = np.linspace(0.0, N/tup, 100)
+
+fig, ax = plt.subplots()
+ax.plot(x_fft, 1./N * np.abs(y_fft[:N]))
+plt.title('Fast Fourier Transform')
+plt.xlabel('frequency')
+plt.ylabel('amplitude')
